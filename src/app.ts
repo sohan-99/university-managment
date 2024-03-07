5aa9b8422388d1f97e8642e0268a47d62340d5c3
sohan-99 <sohan75632@gmail.com>
2024-03-08 01:30:02 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-                 app.use(cors())
+app.use(cors())
 
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing purposes npx prettier --write src/index.ts
-app.get('/', (_req: Request, res: any) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Working successfully')
 })
 
