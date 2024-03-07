5a686c45acf3cf91c29e69e22b541768a4c2fdb9
sohan-99 <sohan75632@gmail.com>
2024-03-08 01:26:50 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app : Application = express();
-const port = 3000
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// Testing purposes
-app.get('/', (_req:Request, res:Response) => {
-  res.send('Working successfully');
-
-  
+// Testing purposes npx prettier --write src/index.ts
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Working successfully')
 })
 
-export default app;
+export default app
