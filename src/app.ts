e481a3c57c27289affc454d17e81ef943171becd
sohan-99 <sohan75632@gmail.com>
2024-03-11 01:44:59 +0600

    [DONE] create user service

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 app.use(cors())
-
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
 // Testing purposes npx prettier --write src/index.ts
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', async (_req: Request, res: Response) => {
   res.send('Working successfully')
 })
 
