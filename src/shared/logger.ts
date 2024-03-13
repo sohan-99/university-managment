53eab513b3afda5572a19ad5b04cdee1acaa98e6
sohan-99 <sohan75632@gmail.com>
2024-03-13 16:16:23 +0600

    [Done] w inston

---
+++ src/shared/logger.ts
@@ -0,0 +1,27 @@
+import winston from 'winston'
+import path from 'path'
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+
+const errorlogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errorlogger }
