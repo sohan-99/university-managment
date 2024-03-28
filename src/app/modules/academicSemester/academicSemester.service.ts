d62acc38deb04bd8f67a52ca0d73114ddb28368d
sohan-99 <sohan75632@gmail.com>
2024-03-28 23:56:01 +0600

    Implement Dynamic Sorting

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -5,6 +5,8 @@ import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemesterModel';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IGenecicResponse } from '../../../interfaces/common';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { SortOrder } from 'mongoose';
 
 const createSemester = async (
   payload: IAcademicSemester,
@@ -18,10 +20,20 @@ const createSemester = async (
 
 const getAllSemesters = async (
   paginationOptions: IPaginationOptions,
-): Promise<IGenecicResponse<IAcademicSemester>> => {
-  const { page = 1, limit = 10 } = paginationOptions;
-  const skip = (page - 1) * limit;
-  const result = await AcademicSemester.find().sort().skip(skip).limit(limit);
+): Promise<IGenecicResponse<IAcademicSemester[]>> => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const result = await AcademicSemester.find()
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
 
   const total = await AcademicSemester.countDocuments();
   return {
